1e84691fb74067208ece0b2c714b38fc387bae40
CloudDev-Harsh <dev.harshpal@gmail.com>
2023-10-15 18:25:19 +0530

    added pointcuts

--- src/main/java/demo/LoggingAspect.java
+++ src/main/java/demo/LoggingAspect.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     @Before("execution(* demo.ShoppingCart.checkout())")
-    public void logger(){
+    public void beforelogger(){
         System.out.println("logger");
     }
 
+    @After("execution(* demo.ShoppingCart.checkout())")
+    public void afterlogger(){
+        System.out.println("logger");
+    }
 }
